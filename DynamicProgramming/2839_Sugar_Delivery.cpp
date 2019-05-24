fde3c35743eeb39ddef8981d1b857b6575f2150b
skyil7 <giopaik@naver.com>
2019-05-24 14:52:57 +0900

    commit

---
+++ DynamicProgramming/2839_Sugar_Delivery.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//problem from https://www.acmicpc.net/problem/2163
+
+int main()
+{
+	
+	int n, m;
+	
+	cin >> n;
+	cin >> m;
+
+	cout << n * m - 1;
+
+	return 0;
+}
